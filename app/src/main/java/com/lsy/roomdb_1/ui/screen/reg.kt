4b6f4ed56811598b2e05df2b6bbe540cd08d7f44
이상엽 <infightersy@naver.com>
2025-04-10 08:27:15 +0900

    등록화면 내용 추가

--- app/src/main/java/com/lsy/roomdb_1/ui/screen/reg.kt
+++ app/src/main/java/com/lsy/roomdb_1/ui/screen/reg.kt
@@ -1,11 +1,22 @@
 package com.lsy.roomdb_1.ui.screen
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,18 +27,59 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.lsy.roomdb_1.data.db.entity.UserEntity
 import com.lsy.roomdb_1.ui.viewmodel.UserViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun reg(navController: NavController, viewModel: UserViewModel) {
-
-    val context = LocalContext.current
-
     var name by remember { mutableStateOf("") }
     var age by remember { mutableStateOf("") }
+    val context = LocalContext.current
 
     Column(modifier = Modifier.fillMaxSize().padding(20.dp), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
 
+        TopAppBar(
+            title = { Text("등록 화면") },
+            navigationIcon = {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                }
+            }
+        )
+
+        TextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("이름") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.padding(top = 16.dp))
+
+        TextField(
+            value = age,
+            onValueChange = { name = it },
+            label = { Text("나이") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.padding(top = 16.dp))
+
+        Button(onClick = {
+                if(name.isNotBlank() && age.isNotBlank()) {
+                    val userEntity = UserEntity(name = name, age = age.toInt())
+                    viewModel.insert(userEntity)
+                    Toast.makeText(context, "저장 완료", Toast.LENGTH_SHORT).show()
+                    navController.navigate("home")
+                } else {
+                    Toast.makeText(context, "이름과 나이를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                }
+        },
+        modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("저장")
+        }
     }
 }
 
